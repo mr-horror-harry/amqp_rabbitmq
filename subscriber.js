0c2d11fb92f9fa22ef099bd998be50c9b95bb3b2
M A Harish J <mr.horror3737@gmail.com>
2024-03-07 03:39:46 +0000

    subscriber module success

--- subscriber.js
+++ subscriber.js
@@ -11,15 +11,14 @@ amqp.connect('amqp://0.0.0.0', (err, connection) => {
             throw err;
         }
         let queueName = "test_queue_1"
-        let mssg = "Hello this is user 2 !"
         // set properties to queue 
         channel.assertQueue(queueName, {
             durable: false
         })
         // send data to queue 
-        channel.sendToQueue(queueName, Buffer.from(mssg))
-        setTimeout(() => {
-            connection.close()
-        }, 1000)
+        channel.consume(queueName, (mssg) => {
+            console.log(`Message from publisher: ${mssg.content.toString()}`)
+            channel.ack(mssg)
+        })
     })
 })
